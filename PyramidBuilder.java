d854f794448e4173d30b265aa750ec2ec66a9980
Matt Knicos <mknicos@gmail.com>
2013-10-29 12:49:51 -0500

    First row of bricks built

--- PyramidBuilder.java
+++ PyramidBuilder.java
@@ -1,5 +1,5 @@
 //	Matt Knicos
-//	mkncios@gmail.com
+//	mknicos@gmail.com
 
 import acm.program.*;
 import acm.graphics.*;
@@ -14,15 +14,33 @@ import acm.graphics.*;
 
 
 public class PyramidBuilder extends GraphicsProgram {
-	public void run(){
-		double x = (getWidth() - BRICK_WIDTH) / 2;
-		double y = getHeight() - BRICK_HEIGHT;
-		GRect brick = new GRect (x,y, BRICK_WIDTH, BRICK_HEIGHT);
-		add(brick);
-	}
 
-private static final int BRICK_WIDTH = 30;
-private static final int BRICK_HEIGHT = 12;
-private static final int BRICK_IN_BASE = 14;
+	// Private constants
 	
+	// Width of each brick in pixels
+private static final int BRICK_WIDTH = 30;
+
+	// Height of each brick in pixels
+private static final int BRICK_HEIGHT = 12;
+
+	// Number of bricks in the bottom row
+private static final int BRICK_IN_BASE = 14;
+
+public void run(){
+
+// Find the starting x,y of the first row of bricks
+	int xMid = getWidth() / 2;
+	int xStart = xMid - ((BRICK_WIDTH * BRICK_IN_BASE) / 2);
+	int yStart = (getHeight() - BRICK_HEIGHT) - 2; // - 2 Allows bottom line of lowest row of bricks to be visible
+	
+// Build one row of bricks
+	int bricksInRow = BRICK_IN_BASE;
+	for (int i = 0; i < bricksInRow; i++){
+		GRect brick = new GRect(xStart, yStart, BRICK_WIDTH, BRICK_HEIGHT);	
+		add(brick);
+		xStart+=BRICK_WIDTH;
+	}
+	
+}
+
 }

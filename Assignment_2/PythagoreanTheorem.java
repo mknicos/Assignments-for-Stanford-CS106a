4eebdbc503e129810cdf23dff2b1ad2c135f1365
Matt Knicos <mknicos@gmail.com>
2013-10-30 13:50:15 -0500

    Program was written as a Graphics Program, program complete

--- Assignment_2/PythagoreanTheorem.java
+++ Assignment_2/PythagoreanTheorem.java
@@ -4,11 +4,12 @@
 import acm.program.*;
 @SuppressWarnings("serial")
 // The Pythagorean Theorem states a^2 + b^2 = c^2  where ^2 means squared. This program will ask the user
-// for "a" and "b" and print the the screen the value of "c".
+// for "a" and "b" and print the the screen the value of "c". Its a dialog program put tested and works
+// as well as a console program too.
 
 
 
-public class PythagoreanTheorem extends GraphicsProgram {
+public class PythagoreanTheorem extends DialogProgram {
 	public void run(){
 		println("Enter values to compute the PythagoreanTheorem.");			//get values from user
 		double a = readDouble("a: ");
@@ -19,6 +20,6 @@ public class PythagoreanTheorem extends GraphicsProgram {
 		double c = a + b;
 		c = Math.sqrt(c);
 		
-		println("c = " + c);
+		println("c = " + c);	
 	}
 }
